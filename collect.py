e9c98201f2d36ffa1c063eb827edcc4422d3024b
mming <mming>
2025-09-20 14:39:35 +0800

    update

--- collect.py
+++ collect.py
@@ -15,8 +15,37 @@ def build_request(hearder=None):
     request.proxies.update(http_proxy)
     return request
 
-t=set()
-node_list=set()
+
+t = set()
+node_list = set()
+
+
+def parse_url_subscription(messages):
+    url_list = []
+    reg = r'https://\S+'
+    node_num = 0
+    for message in messages:
+        urls = re.findall(reg, message.get_text(separator=' '))
+        url_list.extend(urls)
+    for url in url_list:
+        try:
+            response = build_request().get(url, timeout=2)
+            text = response.text
+            if len(text.encode("utf8"))%4!=0:
+                print(f'invalid url={url}')
+                continue
+            text = base64.b64decode(text).decode("utf8").strip()
+            for node in text.splitlines():
+                node_list.add(node)
+                node_num += 1
+        except requests.RequestException as e:
+            print(f"请求错误: {e}")
+        except (UnicodeEncodeError) as e:
+            print(f'invalid url={url}')
+            traceback.print_exc()
+    print(f'node_num={node_num}')
+    return node_num
+
 
 def scrape_node(url):
     try:
@@ -26,19 +55,21 @@ def scrape_node(url):
         # 解析HTML内容
         soup = BeautifulSoup(response.text, 'html.parser')
         messages = soup.find_all('div', attrs={'class': 'tgme_widget_message_text js-message_text'})
-        node_num=0
+        node_num = 0
         reg = r'((vless|vmess|trojan|ss|ssr|hysteria)://[^\s]+)'
         for message in messages:
-            text=message.get_text(separator=' ')
+            text = message.get_text(separator=' ')
             # print(text)
-            nodes=re.findall(reg,text)
-            node_num+= len(nodes)
+            nodes = re.findall(reg, text)
+            node_num += len(nodes)
             for node in nodes:
                 node_list.add(node[0])
                 # print(node[0])
                 pass
-        if node_num==0:
-            t.add(url)
+        if node_num == 0:
+            n = parse_url_subscription(messages)
+            if n == 0:
+                t.add(url)
         return
     except requests.RequestException as e:
         print(f"请求错误: {e}")
@@ -49,16 +80,15 @@ def scrape_node(url):
         return
 
 
-
 def load_config(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         # 使用 yaml.safe_load() 加载文件内容
         # safe_load() 比 load() 更安全，因为它只解析有限的 YAML 标签
         # 避免执行恶意代码
         data = yaml.safe_load(file)
-        url_list=data.get('tgchannel')
+        url_list = data.get('tgchannel')
         for url in url_list:
-            url='https://t.me/s/'+url.replace('https://t.me/','')
+            url = 'https://t.me/s/' + url.replace('https://t.me/', '')
             scrape_node(url)
 
 
@@ -67,30 +97,28 @@ def get_valid_nodes():
     try:
         response = build_request().get(url)
         response.raise_for_status()
-        text=response.text
-        text=base64.b64decode(text).decode("utf8").strip()
-        text='\n'.join(line for line in text.splitlines() if line.strip())
+        text = response.text
+        text = base64.b64decode(text).decode("utf8").strip()
+        text = '\n'.join(line for line in text.splitlines() if line.strip())
         return text
     except requests.RequestException as e:
         print(f"请求错误: {e}")
         return ''
+
 
 if __name__ == "__main__":
     load_config('./config.yaml')
     print('-------')
     for url in t:
         print(url)
-    with open('valid_content_all.txt','w+',encoding='utf8') as f:
+    with open('valid_content_all.txt', 'w+', encoding='utf8') as f:
         # for n in f:
         #     node_list.add(n.strip())
         # f.seek(0)
-        text=get_valid_nodes()
+
+        text = get_valid_nodes()
         f.write(text)
         f.write('\n')
         for node in node_list:
             f.write(node)
             f.write('\n')
-
-
-
-
